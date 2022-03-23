26f392b758b24200bf5cbca83bc185ba6a67e2de
ajlekcahdp4 <alex.rom23@mail.ru>
2022-03-23 14:31:30 +0300

    Input check created

---
+++ DAC/input_check.py
@@ -0,0 +1,25 @@
+def isfloat (val_str):
+    try:
+        float (val_str)
+        return True
+    except ValueError:
+        return False
+
+
+
+def InputCheck (val_str):
+    if val_str.isdigit():
+        if int(val_str) > 255:
+            print ("ERROR: 45Was entered the number greater then 255\n")
+            return 0
+        return 1
+    elif isfloat(val_str):
+        val_float = float (val_str)
+        if val_float < 0:
+            print ("ERROR: anegative number was entered\n")
+        else:
+            print ("ERROR: float number was entered\n")
+        return 0
+    elif val_str != "q":
+        print ("ERROR: Not a number was entered\n")
+        return 0
